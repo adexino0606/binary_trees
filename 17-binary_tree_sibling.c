d76e5f897af6af19dc9a6977d7d7d7635f184ecd
adexino0606 <adexino0606@github.com>
2022-08-25 19:35:44 -0700

    update

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,19 +1,19 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_sibling - Function that checks if a binary tree is perfect
+ * binary_tree_sibling - returns the sibling of the node
+ * @node: the node to find sibling of
  *
- * @node: Pointer to a node of a binary tree.
- *
- * Return: Pointer to sibling or NULL.
+ * Return: returns the sibling of the node
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node || !(node->parent))
+	if (!node || !node->parent)
 		return (NULL);
 
-	if (node == node->parent->left)
+	if (node->parent->right == node && node->parent->left)
+		return (node->parent->left);
+	if (node->parent->left == node && node->parent->right)
 		return (node->parent->right);
-	return (node->parent->left);
-}	
+	return (NULL);
+}
